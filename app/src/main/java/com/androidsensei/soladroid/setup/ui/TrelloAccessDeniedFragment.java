4ea100bccd9c3b5c40c02be827a244f99fece452
Mihai Campean <sensei@androidsensei.com>
2015-05-22 22:42:14 +0300

    added the action on the ok button for the access denied screen

--- app/src/main/java/com/androidsensei/soladroid/setup/ui/TrelloAccessDeniedFragment.java
+++ app/src/main/java/com/androidsensei/soladroid/setup/ui/TrelloAccessDeniedFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.androidsensei.soladroid.R;
 import com.androidsensei.soladroid.utils.SolaDroidBaseFragment;
@@ -22,5 +23,20 @@ public class TrelloAccessDeniedFragment extends SolaDroidBaseFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        setupOkButton((Button) getView().findViewById(R.id.trello_denied_ok_button));
+    }
+
+    /**
+     * Sets up what happens when you click the "ok" button, namely the authorization process should be re-started.
+     *
+     * @param button the button to set the click listener on
+     */
+    private void setupOkButton(Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contract.showAuthFragment();
+            }
+        });
     }
 }
